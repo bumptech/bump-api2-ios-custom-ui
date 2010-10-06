17066d9967305e6265ee80cdf63bb331dfb1f01d
Jake Kerr <jkerr@bumptechnologies.com>
2010-10-06 16:37:54 -0700

    Update license

--- BumpAPIWaitPage.h
+++ BumpAPIWaitPage.h
@@ -2,18 +2,30 @@
 //  BumpAPIWaitPage.h
 //  BumpAPI
 //
-//  Copyrights / Disclaimer
-//  Copyright 2010, Bump Technologies, Inc. All rights reserved.
-//  Use of the software programs described herein is subject to applicable
-//  license agreements and nondisclosure agreements. Unless specifically
-//  otherwise agreed in writing, all rights, title, and interest to this
-//  software and documentation remain with Bump Technologies, Inc. Unless
-//  expressly agreed in a signed license agreement, Bump Technologies makes
-//  no representations about the suitability of this software for any purpose
-//  and it is provided "as is" without express or implied warranty.
+//  Copyright (c) 2010, Bump Technologies, Inc.
+//  All rights reserved.
 //
-//  Copyright (c) 2010 Bump Technologies Inc. All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//      * Neither the name of Bump Technologies, Inc. nor the
+//        names of its contributors may be used to endorse or promote products
+//        derived from this software without specific prior written permission.
 //
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import <UIKit/UIKit.h>
 
